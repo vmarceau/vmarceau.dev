552e85c66e1f5f4d8ad88e60f3866d93ca16432e
vmarceau <vmarceau@gmail.com>
2022-10-20 08:57:05 -0400

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -10,24 +10,25 @@ export default function About() {
       <div className={classes.content}>
         <h2>About me</h2>
         <p>
-          I followed a slightly unusual professional trajectory, that brought me from physics to AI/ML and nowadays to
-          software engineering, a field I am very passionate about.
+          I followed a slightly unusual professional trajectory, that brought me from academic research in computational
+          physics to software engineering, a field I am very passionate about.
         </p>
         <p>
           I currently work at{' '}
           <a href={'https://www.auth0.com'} target={'_blank'} rel={'noopener noreferrer'}>
             Auth0
           </a>{' '}
-          (part of Okta) as a software engineer, where I am building at the intersection of large-scale distributed systems,
-          security, identity and access management.
+          (part of Okta) as a software engineer, where I am building at the intersection of large-scale distributed
+          systems, security, identity and access management.
         </p>
         <p>
           Before, I worked for a few years as a machine learning and software engineer at{' '}
           <a href={'https://www.xpertsea.com'} target={'_blank'} rel={'noopener noreferrer'}>
             XpertSea
           </a>
-          , a tech startup aiming to improve aquaculture practices. This was an incredible experience and I got the
-          opportunity to learn many skills. Prior to that, I started my journey in the industry at{' '}
+          , a dynamic startup company aiming to improve the world's aquaculture practices through the use of technology.
+          This was an incredible experience and I got the opportunity to contribute and learn on many fronts. Prior to
+          that, I started my journey in the industry at{' '}
           <a href={'https://www.abb.com'} target={'_blank'} rel={'noopener noreferrer'}>
             ABB
           </a>
